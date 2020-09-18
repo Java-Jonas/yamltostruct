ecb8f480bc6aa47d853a1c774993690d359f72f8
Java-Jonas <jonas.bergner96@googlemail.com>
2020-09-18 17:32:20 +0200

    wip: type declaration completeness

--- validation.go
+++ validation.go
@@ -3,7 +3,10 @@ package yamltostruct
 import (
 	"fmt"
 	"reflect"
+	"regexp"
 )
+
+var golangBasicTypes = []string{"string", "bool", "int8", "uint8", "byte", "int16", "uint16", "int32", "rune", "uint32", "int64", "uint64", "int", "uint", "uintptr", "float32", "float64", "complex64", "complex128"}
 
 func isString(unknown interface{}) bool {
 	v := reflect.ValueOf(unknown)
@@ -49,7 +52,7 @@ func validateValues(yamlData map[interface{}]interface{}) (errs []error) {
 				errs = append(errs, newUnexpectedError())
 				continue
 			}
-			objectValidationErrs := validateObject(mapValue, keyName)
+			objectValidationErrs := validateObjectValues(mapValue, keyName)
 			errs = append(errs, objectValidationErrs...)
 			continue
 		}
@@ -60,7 +63,7 @@ func validateValues(yamlData map[interface{}]interface{}) (errs []error) {
 	return
 }
 
-func validateObject(yamlObjectData map[interface{}]interface{}, parentObjectName string) (errs []error) {
+func validateObjectValues(yamlObjectData map[interface{}]interface{}, parentObjectName string) (errs []error) {
 	for key, value := range yamlObjectData {
 		keyName := fmt.Sprintf("%v", key)
 
@@ -74,6 +77,80 @@ func validateObject(yamlObjectData map[interface{}]interface{}, parentObjectName
 		}
 
 		errs = append(errs, newValidationErrorInvalidValue(keyName, parentObjectName))
+	}
+
+	return
+}
+
+// extracts all types which are defined in a type definition
+// map[string]int => []string{"string", "int"}
+// (?!map\b)\b\w+/g
+func extractTypes(typeDefinitionString string) (extractedTypes []string) {
+	re := regexp.MustCompile(`(?!map\b)\b\w+/g`)
+	// TODO
+	re.FindString(typeDefinitionString)
+	return
+}
+
+func findUndefinedTypesIn(usedTypes, definedTypes []string) (undefinedTypes []string) {
+	return
+}
+
+func validateTypeDeclarationCompleteness(yamlData map[interface{}]interface{}) (errs []error) {
+
+	var definedTypes []string
+
+	for key := range yamlData {
+		keyName := fmt.Sprintf("%v", key)
+		if keyName == "_package" {
+			continue
+		}
+		definedTypes = append(definedTypes, keyName)
+	}
+
+	for key, value := range yamlData {
+		keyName := fmt.Sprintf("%v", key)
+
+		if keyName == "_package" {
+			continue
+		}
+
+		if isString(value) {
+			valueString := fmt.Sprintf("%v", value)
+			extractedTypes := extractTypes(valueString)
+			undefinedTypes := findUndefinedTypesIn(extractedTypes, definedTypes)
+			for _, undefiundefinedType := range undefinedTypes {
+				errs = append(errs, newValidationErrorTypeNotFound(undefiundefinedType, "root"))
+			}
+		}
+
+		if isMap(value) {
+			mapValue, ok := value.(map[interface{}]interface{})
+			if !ok {
+				errs = append(errs, newUnexpectedError())
+				continue
+			}
+			objectValidationErrs := validateObjectTypesDeclarationCompleteness(mapValue, keyName, definedTypes)
+			errs = append(errs, objectValidationErrs...)
+		}
+	}
+
+	return
+}
+
+func validateObjectTypesDeclarationCompleteness(
+	yamlObjectData map[interface{}]interface{},
+	parentObjectName string,
+	definedTypes []string,
+) (errs []error) {
+
+	for _, value := range yamlObjectData {
+		valueString := fmt.Sprintf("%v", value)
+		extractedTypes := extractTypes(valueString)
+		undefinedTypes := findUndefinedTypesIn(extractedTypes, definedTypes)
+		for _, undefiundefinedType := range undefinedTypes {
+			errs = append(errs, newValidationErrorTypeNotFound(undefiundefinedType, parentObjectName))
+		}
 	}
 
 	return
