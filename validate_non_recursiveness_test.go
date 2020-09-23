3005fa4ac879fd19c5db0dc99d6506edf77924a9
Java-Jonas <jonas.bergner96@googlemail.com>
2020-09-23 10:50:48 +0200

    consistent file structure

--- validate_non_recursiveness_test.go
+++ validate_non_recursiveness_test.go
@@ -1,1 +1,99 @@
 package yamltostruct
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateYamlRecursiveTypeUsage(t *testing.T) {
+	t.Run("should not fail on usage of non-recursive types", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"bar": map[interface{}]interface{}{
+				"foo": "baz",
+			},
+			"baz": map[interface{}]interface{}{
+				"ban": "*bar",
+			},
+			"bal": map[interface{}]interface{}{
+				"bam": "baz",
+			},
+		}
+
+		actualErrors := validateYamlData(data)
+		expectedErrors := []error{}
+
+		missingErrors, redundantErrors := matchErrors(actualErrors, expectedErrors)
+
+		assert.Equal(t, []error{}, missingErrors)
+		assert.Equal(t, []error{}, redundantErrors)
+	})
+
+	t.Run("should fail when type is used in own declaration", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"bar":      "bar",
+			"baz": map[interface{}]interface{}{
+				"ban": "baz",
+			},
+		}
+
+		actualErrors := validateYamlData(data)
+		expectedErrors := []error{
+			newValidationErrorRecursiveTypeUsage([]string{"ban"}),
+		}
+
+		missingErrors, redundantErrors := matchErrors(actualErrors, expectedErrors)
+
+		assert.Equal(t, []error{}, missingErrors)
+		assert.Equal(t, []error{}, redundantErrors)
+	})
+
+	t.Run("should fail on usage of recursive types (1/2)", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"bar": map[interface{}]interface{}{
+				"foo": "baz",
+			},
+			"baz": map[interface{}]interface{}{
+				"ban": "bar",
+			},
+		}
+
+		actualErrors := validateYamlData(data)
+		expectedErrors := []error{
+			newValidationErrorRecursiveTypeUsage([]string{"foo", "ban"}),
+		}
+
+		missingErrors, redundantErrors := matchErrors(actualErrors, expectedErrors)
+
+		assert.Equal(t, []error{}, missingErrors)
+		assert.Equal(t, []error{}, redundantErrors)
+	})
+
+	t.Run("should fail on usage of recursive types (2/2)", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"bar": map[interface{}]interface{}{
+				"foo": "bam",
+			},
+			"baz": map[interface{}]interface{}{
+				"ban": "bar",
+			},
+			"bam": map[interface{}]interface{}{
+				"baf": "baz",
+			},
+		}
+
+		actualErrors := validateYamlData(data)
+		expectedErrors := []error{
+			newValidationErrorRecursiveTypeUsage([]string{"baf", "ban", "foo"}),
+		}
+
+		missingErrors, redundantErrors := matchErrors(actualErrors, expectedErrors)
+
+		assert.Equal(t, []error{}, missingErrors)
+		assert.Equal(t, []error{}, redundantErrors)
+	})
+}
