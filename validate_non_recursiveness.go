37736174c4263c83f28a2ab6694e709fd5234e3b
Java-Jonas <jonas.bergner96@googlemail.com>
2020-09-24 11:18:18 +0200

    wip: recursiveness check

--- validate_non_recursiveness.go
+++ validate_non_recursiveness.go
@@ -1,5 +1,67 @@
 package yamltostruct
 
+import (
+	"fmt"
+)
+
+type declarationBranch []string
+type declarationTree struct {
+	yamlData map[interface{}]interface{}
+	branches []declarationBranch
+}
+
+func (db declarationBranch) copySelf() declarationBranch {
+	branchCopy := make(declarationBranch, len(db))
+	copy(branchCopy, db)
+	return branchCopy
+}
+
+func (db declarationBranch) wouldCreateLoopWith(keyName string) (wouldCreateLoop bool) {
+
+	for _, segment := range db {
+		if segment == keyName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (tree *declarationTree) addBranch(branch declarationBranch) {
+	tree.branches = append(tree.branches, branch)
+}
+
+func (tree *declarationTree) grow(branch declarationBranch, keyName string, value interface{}) {
+
+	if isString(value) {
+		if branch.wouldCreateLoopWith(keyName) {
+			branch = append(branch, keyName)
+			tree.addBranch(branch)
+			return
+		}
+		branch = append(branch, keyName)
+		valueLiteral := fmt.Sprintf("%v", value)
+		nextValue, ok := tree.yamlData[valueLiteral]
+		if !ok {
+			branch = append(branch, valueLiteral)
+			tree.addBranch(branch)
+			return
+		}
+
+		tree.grow(branch, valueLiteral, nextValue)
+	}
+
+	if isMap(value) {
+		branch = append(branch, keyName+".")
+		mapValue := value.(map[interface{}]interface{})
+		for _key, _value := range mapValue {
+			branchCopy := branch.copySelf()
+			_keyName := fmt.Sprintf("%v", _key)
+			tree.grow(branchCopy, _keyName, _value)
+		}
+	}
+}
+
 func validateNonRecursiveness(yamlData map[interface{}]interface{}) (errs []error) {
 	return
 }
--- validate_non_recursiveness_test.go
+++ validate_non_recursiveness_test.go
@@ -41,7 +41,8 @@ func TestValidateYamlRecursiveTypeUsage(t *testing.T) {
 
 		actualErrors := validateYamlData(data)
 		expectedErrors := []error{
-			newValidationErrorRecursiveTypeUsage([]string{"ban"}),
+			newValidationErrorRecursiveTypeUsage([]string{"bar"}),
+			newValidationErrorRecursiveTypeUsage([]string{"baz.ban"}),
 		}
 
 		missingErrors, redundantErrors := matchErrors(actualErrors, expectedErrors)
@@ -50,7 +51,7 @@ func TestValidateYamlRecursiveTypeUsage(t *testing.T) {
 		assert.Equal(t, []error{}, redundantErrors)
 	})
 
-	t.Run("should fail on usage of recursive types (1/2)", func(t *testing.T) {
+	t.Run("should fail on usage of recursive types (1/3)", func(t *testing.T) {
 		data := map[interface{}]interface{}{
 			"_package": "packageName",
 			"bar": map[interface{}]interface{}{
@@ -63,7 +64,7 @@ func TestValidateYamlRecursiveTypeUsage(t *testing.T) {
 
 		actualErrors := validateYamlData(data)
 		expectedErrors := []error{
-			newValidationErrorRecursiveTypeUsage([]string{"foo", "ban"}),
+			newValidationErrorRecursiveTypeUsage([]string{"bar.foo", "baz.ban"}),
 		}
 
 		missingErrors, redundantErrors := matchErrors(actualErrors, expectedErrors)
@@ -72,7 +73,7 @@ func TestValidateYamlRecursiveTypeUsage(t *testing.T) {
 		assert.Equal(t, []error{}, redundantErrors)
 	})
 
-	t.Run("should fail on usage of recursive types (2/2)", func(t *testing.T) {
+	t.Run("should fail on usage of recursive types (2/3)", func(t *testing.T) {
 		data := map[interface{}]interface{}{
 			"_package": "packageName",
 			"bar": map[interface{}]interface{}{
@@ -88,12 +89,88 @@ func TestValidateYamlRecursiveTypeUsage(t *testing.T) {
 
 		actualErrors := validateYamlData(data)
 		expectedErrors := []error{
-			newValidationErrorRecursiveTypeUsage([]string{"baf", "ban", "foo"}),
+			newValidationErrorRecursiveTypeUsage([]string{"bam.baf", "baz.ban", "bar.foo"}),
 		}
 
 		missingErrors, redundantErrors := matchErrors(actualErrors, expectedErrors)
 
 		assert.Equal(t, []error{}, missingErrors)
 		assert.Equal(t, []error{}, redundantErrors)
+	})
+}
+
+func TestDeclarationTreeGrow(t *testing.T) {
+	t.Run("should build one branch of flat types and exit on loop", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"foo":      "bar",
+			"ban":      "foo",
+			"bar":      "ban",
+		}
+
+		dt := declarationTree{
+			branches: []declarationBranch{},
+			yamlData: data,
+		}
+
+		dt.grow(declarationBranch{}, "ban", data["ban"])
+
+		assert.Equal(t, 1, len(dt.branches))
+		assert.Equal(t, declarationBranch{"ban", "foo", "bar", "ban"}, dt.branches[0])
+	})
+
+	t.Run("should build one branch of flat types that exits on a basic type", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"foo":      "bar",
+			"bar":      "string",
+		}
+
+		dt := declarationTree{
+			branches: []declarationBranch{},
+			yamlData: data,
+		}
+
+		dt.grow(declarationBranch{}, "foo", data["foo"])
+
+		assert.Equal(t, 1, len(dt.branches))
+		assert.Equal(t, declarationBranch{"foo", "bar", "string"}, dt.branches[0])
+	})
+
+	t.Run("should build one branch of a flat type directly", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"bar":      "string",
+		}
+
+		dt := declarationTree{
+			branches: []declarationBranch{},
+			yamlData: data,
+		}
+
+		dt.grow(declarationBranch{}, "bar", data["bar"])
+
+		assert.Equal(t, 1, len(dt.branches))
+		assert.Equal(t, declarationBranch{"bar", "string"}, dt.branches[0])
+	})
+
+	t.Run("should build one branch of a flat type directly", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"bar": map[interface{}]interface{}{
+				"foo": "baz",
+			},
+			"baz": "bar",
+		}
+
+		dt := declarationTree{
+			branches: []declarationBranch{},
+			yamlData: data,
+		}
+
+		dt.grow(declarationBranch{}, "baz", data["baz"])
+
+		assert.Equal(t, 1, len(dt.branches))
+		assert.Equal(t, declarationBranch{"baz", "bar.", "foo", "baz"}, dt.branches[0])
 	})
 }
