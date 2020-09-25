4b8a79bafd9638e5a81fcdb6bd480f83c930bceb
Java-Jonas <jonas.bergner96@googlemail.com>
2020-09-25 12:32:49 +0200

    wip: more recursive check development

--- validate_non_recursiveness.go
+++ validate_non_recursiveness.go
@@ -4,64 +4,122 @@ import (
 	"fmt"
 )
 
-type declarationBranch []string
-type declarationTree struct {
-	yamlData map[interface{}]interface{}
-	branches []declarationBranch
+type valueKind int
+
+const (
+	valueKindString valueKind = iota
+	valueKindObject
+)
+
+type fieldLevelKind int
+
+const (
+	fieldLevelZero fieldLevelKind = iota
+	firstFieldLevel
+	secondFieldLevel
+)
+
+type declarationSegment struct {
+	typeName   string
+	valueType  valueKind
+	fieldLevel fieldLevelKind
 }
 
-func (db declarationBranch) copySelf() declarationBranch {
-	branchCopy := make(declarationBranch, len(db))
-	copy(branchCopy, db)
-	return branchCopy
+type declarationBranch struct {
+	segments              []declarationSegment
+	containsRecursiveness bool
 }
 
-func (db declarationBranch) wouldCreateLoopWith(keyName string) (wouldCreateLoop bool) {
+func (branch *declarationBranch) addSegment(typeName string, valueType valueKind, fieldLevel fieldLevelKind) {
+	for _, segment := range branch.segments {
+		if segment.typeName == typeName {
+			branch.containsRecursiveness = true
+		}
+	}
+	branch.segments = append(branch.segments, declarationSegment{typeName, valueType, fieldLevel})
+}
 
-	for _, segment := range db {
-		if segment == keyName {
-			return true
+func (branch declarationBranch) declarationPath() []string {
+	var path []string
+
+	var isStructField bool
+	var parentStructName string
+
+	for _, segment := range branch.segments {
+		if segment.valueType == valueKindObject {
+			isStructField = true
+			parentStructName = segment.typeName
+			continue
+		}
+		if isStructField {
+			path = append(path, parentStructName+"."+segment.typeName)
+			isStructField = false
+			parentStructName = ""
+		} else {
+			path = append(path, segment.typeName)
 		}
 	}
 
-	return false
+	return path
+}
+
+func (branch declarationBranch) copySelf() declarationBranch {
+	segmentsCopy := make([]declarationSegment, len(branch.segments))
+	copy(segmentsCopy, branch.segments)
+	branchCopy := branch
+	branchCopy.segments = segmentsCopy
+	return branchCopy
+}
+
+type declarationTree struct {
+	yamlData map[interface{}]interface{}
+	branches []declarationBranch
 }
 
 func (tree *declarationTree) addBranch(branch declarationBranch) {
 	tree.branches = append(tree.branches, branch)
 }
 
-func (tree *declarationTree) grow(branch declarationBranch, keyName string, value interface{}) {
+func (tree *declarationTree) grow(branch declarationBranch, keyName string, value interface{}, fieldLevel fieldLevelKind) {
+
+	if keyName == "_package" {
+		return
+	}
 
 	if isString(value) {
-		if branch.wouldCreateLoopWith(keyName) {
-			branch = append(branch, keyName)
+		branch.addSegment(keyName, valueKindString, fieldLevel)
+		if branch.containsRecursiveness {
 			tree.addBranch(branch)
 			return
 		}
-		branch = append(branch, keyName)
 		valueLiteral := fmt.Sprintf("%v", value)
-		nextValue, ok := tree.yamlData[valueLiteral]
-		if !ok {
-			branch = append(branch, valueLiteral)
+		nextValue, isNotBasicType := tree.yamlData[valueLiteral]
+		if !isNotBasicType {
+			branch.addSegment(valueLiteral, valueKindString, fieldLevelZero)
 			tree.addBranch(branch)
 			return
 		}
 
-		tree.grow(branch, valueLiteral, nextValue)
+		tree.grow(branch, valueLiteral, nextValue, firstFieldLevel)
 	}
 
 	if isMap(value) {
-		branch = append(branch, keyName+".")
+		branch.addSegment(keyName, valueKindObject, fieldLevel)
+		if branch.containsRecursiveness {
+			tree.addBranch(branch)
+			return
+		}
 		mapValue := value.(map[interface{}]interface{})
 		for _key, _value := range mapValue {
 			branchCopy := branch.copySelf()
 			_keyName := fmt.Sprintf("%v", _key)
-			tree.grow(branchCopy, _keyName, _value)
+			tree.grow(branchCopy, _keyName, _value, fieldLevel+1)
 		}
 	}
 }
 
 func validateNonRecursiveness(yamlData map[interface{}]interface{}) (errs []error) {
+	// var tree declarationTree
+	// tree.grow()
 	return
 }
--- validate_non_recursiveness_test.go
+++ validate_non_recursiveness_test.go
@@ -100,6 +100,28 @@ func TestValidateYamlRecursiveTypeUsage(t *testing.T) {
 }
 
 func TestDeclarationTreeGrow(t *testing.T) {
+	t.Run("should build segments with expected fieldLevels", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"foo": map[interface{}]interface{}{
+				"bar": "string",
+			},
+		}
+
+		dt := declarationTree{
+			branches: []declarationBranch{},
+			yamlData: data,
+		}
+
+		dt.grow(declarationBranch{}, "", data, fieldLevelZero)
+
+		assert.Equal(t, 1, len(dt.branches))
+		assert.Equal(t, []string{"foo.bar", "string"}, dt.branches[0].declarationPath())
+		assert.Contains(t, dt.branches[0].segments, declarationSegment{"foo", valueKindObject, firstFieldLevel})
+		assert.Contains(t, dt.branches[0].segments, declarationSegment{"bar", valueKindString, secondFieldLevel})
+		assert.Contains(t, dt.branches[0].segments, declarationSegment{"string", valueKindString, fieldLevelZero})
+	})
+
 	t.Run("should build one branch of flat types and exit on loop", func(t *testing.T) {
 		data := map[interface{}]interface{}{
 			"_package": "packageName",
@@ -113,10 +135,11 @@ func TestDeclarationTreeGrow(t *testing.T) {
 			yamlData: data,
 		}
 
-		dt.grow(declarationBranch{}, "ban", data["ban"])
+		dt.grow(declarationBranch{}, "ban", data["ban"], firstFieldLevel)
 
 		assert.Equal(t, 1, len(dt.branches))
-		assert.Equal(t, declarationBranch{"ban", "foo", "bar", "ban"}, dt.branches[0])
+		assert.Equal(t, []string{"ban", "foo", "bar", "ban"}, dt.branches[0].declarationPath())
+		assert.Equal(t, true, dt.branches[0].containsRecursiveness)
 	})
 
 	t.Run("should build one branch of flat types that exits on a basic type", func(t *testing.T) {
@@ -131,10 +154,11 @@ func TestDeclarationTreeGrow(t *testing.T) {
 			yamlData: data,
 		}
 
-		dt.grow(declarationBranch{}, "foo", data["foo"])
+		dt.grow(declarationBranch{}, "foo", data["foo"], firstFieldLevel)
 
 		assert.Equal(t, 1, len(dt.branches))
-		assert.Equal(t, declarationBranch{"foo", "bar", "string"}, dt.branches[0])
+		assert.Equal(t, []string{"foo", "bar", "string"}, dt.branches[0].declarationPath())
+		assert.Equal(t, false, dt.branches[0].containsRecursiveness)
 	})
 
 	t.Run("should build one branch of a flat type directly", func(t *testing.T) {
@@ -148,13 +172,14 @@ func TestDeclarationTreeGrow(t *testing.T) {
 			yamlData: data,
 		}
 
-		dt.grow(declarationBranch{}, "bar", data["bar"])
+		dt.grow(declarationBranch{}, "bar", data["bar"], firstFieldLevel)
 
 		assert.Equal(t, 1, len(dt.branches))
-		assert.Equal(t, declarationBranch{"bar", "string"}, dt.branches[0])
+		assert.Equal(t, []string{"bar", "string"}, dt.branches[0].declarationPath())
+		assert.Equal(t, false, dt.branches[0].containsRecursiveness)
 	})
 
-	t.Run("should build one branch of a flat type directly", func(t *testing.T) {
+	t.Run("should build one branch of nested types", func(t *testing.T) {
 		data := map[interface{}]interface{}{
 			"_package": "packageName",
 			"bar": map[interface{}]interface{}{
@@ -168,9 +193,126 @@ func TestDeclarationTreeGrow(t *testing.T) {
 			yamlData: data,
 		}
 
-		dt.grow(declarationBranch{}, "baz", data["baz"])
+		dt.grow(declarationBranch{}, "baz", data["baz"], firstFieldLevel)
 
 		assert.Equal(t, 1, len(dt.branches))
-		assert.Equal(t, declarationBranch{"baz", "bar.", "foo", "baz"}, dt.branches[0])
+		assert.Equal(t, []string{"baz", "bar.foo", "baz"}, dt.branches[0].declarationPath())
+		assert.Equal(t, true, dt.branches[0].containsRecursiveness)
 	})
+
+	t.Run("should build two branches of nested types", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"bar": map[interface{}]interface{}{
+				"foo": "baz",
+				"bam": "string",
+			},
+			"baz": "bar",
+		}
+
+		dt := declarationTree{
+			branches: []declarationBranch{},
+			yamlData: data,
+		}
+
+		dt.grow(declarationBranch{}, "baz", data["baz"], firstFieldLevel)
+
+		assert.Equal(t, 2, len(dt.branches))
+		declarationPaths := [][]string{
+			dt.branches[0].declarationPath(),
+			dt.branches[1].declarationPath(),
+		}
+
+		assert.Contains(t, declarationPaths, []string{"baz", "bar.foo", "baz"})
+		assert.Contains(t, declarationPaths, []string{"baz", "bar.bam", "string"})
+	})
+
+	t.Run("should build a branch with a itself-referring struct", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"bar": map[interface{}]interface{}{
+				"foo": "bar",
+			},
+		}
+
+		dt := declarationTree{
+			branches: []declarationBranch{},
+			yamlData: data,
+		}
+
+		dt.grow(declarationBranch{}, "bar", data["bar"], firstFieldLevel)
+
+		assert.Equal(t, 1, len(dt.branches))
+		assert.Equal(t, []string{"bar.foo"}, dt.branches[0].declarationPath())
+		assert.Equal(t, true, dt.branches[0].containsRecursiveness)
+	})
+
+	t.Run("should build multiple branches of nested types", func(t *testing.T) {
+		data := map[interface{}]interface{}{
+			"_package": "packageName",
+			"bar": map[interface{}]interface{}{
+				"foo": "baz",
+				"bam": "string",
+				"bal": "bar",
+				"fof": "bas",
+			},
+			"bas": map[interface{}]interface{}{
+				"ban":  "string",
+				"bunt": "bant",
+			},
+			"baz":  "bar",
+			"bant": "int",
+		}
+
+		dt := declarationTree{
+			branches: []declarationBranch{},
+			yamlData: data,
+		}
+
+		dt.grow(declarationBranch{}, "baz", data["baz"], firstFieldLevel)
+
+		assert.Equal(t, 5, len(dt.branches))
+		declarationPaths := [][]string{
+			dt.branches[0].declarationPath(),
+			dt.branches[1].declarationPath(),
+			dt.branches[2].declarationPath(),
+			dt.branches[3].declarationPath(),
+			dt.branches[4].declarationPath(),
+		}
+
+		assert.Contains(t, declarationPaths, []string{"baz", "bar.foo", "baz"})
+		assert.Contains(t, declarationPaths, []string{"baz", "bar.bam", "string"})
+		assert.Contains(t, declarationPaths, []string{"baz", "bar.bal"})
+		assert.Contains(t, declarationPaths, []string{"baz", "bar.fof", "bas.ban", "string"})
+		assert.Contains(t, declarationPaths, []string{"baz", "bar.fof", "bas.bunt", "bant", "int"})
+	})
+
+	// 	t.Run("should build branches from yamlData root", func(t *testing.T) {
+	// 		data := map[interface{}]interface{}{
+	// 			"_package": "packageName",
+	// 			"bar": map[interface{}]interface{}{
+	// 				"foo": "baz",
+	// 				"bam": "string",
+	// 			},
+	// 			"baz": "bar",
+	// 		}
+
+	// 		dt := declarationTree{
+	// 			branches: []declarationBranch{},
+	// 			yamlData: data,
+	// 		}
+
+	// 		dt.grow(declarationBranch{}, "", data, fieldLevelZero)
+
+	// 		assert.Equal(t, 4, len(dt.branches))
+	// 		declarationPaths := [][]string{
+	// 			dt.branches[0].declarationPath(),
+	// 			dt.branches[1].declarationPath(),
+	// 			dt.branches[2].declarationPath(),
+	// 			dt.branches[3].declarationPath(),
+	// 		}
+
+	// 		assert.Contains(t, declarationPaths, []string{"baz", "bar.foo", "baz"})
+	// 		assert.Contains(t, declarationPaths, []string{"baz", "bar.bam", "string"})
+	// 	})
 }
